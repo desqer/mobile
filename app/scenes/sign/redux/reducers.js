de16b2a78750e50eae4158405a5511f753ee0b2a
Diego Schell Fernandes <diego.schell.f@gmail.com>
2017-03-02 11:56:15 -0300

    Remove arquivo desnecessário para a estrutura redux

--- app/scenes/sign/redux/reducers.js
+++
@@ -1,8 +0,0 @@
-import * as types from './types'
-import createReducer from 'app/common/lib/CreateReducer'
-
-export const signUser = createReducer({}, {
-  [types.SET_USER](state, action) {
-    return action.user
-  }
-})
