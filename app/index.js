9a33c3dc2baf669e0f930bcf838c7cfdb0ee241b
Diego Schell Fernandes <diego.schell.f@gmail.com>
2017-03-08 07:01:33 -0300

    Organização das rotas em outro arquivo

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Provider, connect } from 'react-redux'
 import { Scene, Router } from 'react-native-router-flux'
+import scenes from './routes'
 
 /**
  * Middlewares
@@ -33,52 +34,11 @@ const ReduxRouter = connect(
   (dispatch) => bindActionCreators(ActionCreators, dispatch)
 )(Router)
 
-/**
- * Scenes
- */
-import SignPhone from 'app/scenes/sign/scenes/signphone'
-import SignUp from 'app/scenes/sign/scenes/signup'
-import SignIn from 'app/scenes/sign/scenes/signin'
-
-import Calendar from 'app/scenes/calendar'
-import Detail from 'app/scenes/calendar/scenes/detail'
-import New from 'app/scenes/calendar/scenes/new'
-
-import Header from 'app/scenes/calendar/components/Header'
-
-/**
- * Tab icons
- */
-import {
-  ProfileIcon,
-  DesqerIcon,
-  HistoryIcon
-} from 'app/common/components/Footer'
-
 export default class Desqer extends Component {
   render() {
     return (
       <Provider store={store}>
-        <ReduxRouter>
-          <Scene key="root" hideNavBar={true}>
-            <Scene key="signPhone" component={SignPhone} initial={true} />
-            <Scene key="signUp" component={SignUp} />
-            <Scene key="signIn" component={SignIn} />
-
-            <Scene
-              key="app"
-              tabs={true}
-              tabBarStyle={{ height: 60, backgroundColor: '#BE446D' }}
-            >
-              <Scene key="profile" navBar={Header} component={SignPhone} title="Minha conta" icon={ProfileIcon} />
-              <Scene key="appointments" navBar={Header} icon={DesqerIcon} initial={true}>
-                <Scene key="calendar" component={Calendar} />
-                <Scene key="new" component={New} initial={true} title="Criar serviço" />
-              </Scene>
-              <Scene key="history" navBar={Header} component={SignUp} title="Histórico" icon={HistoryIcon} />
-            </Scene>
-          </Scene>
-        </ReduxRouter>
+        <ReduxRouter scenes={scenes} />
       </Provider>
     )
   }
--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,53 @@
-import { ActionConst } from 'react-native-router-flux';
+import React from 'react'
+import { Actions, Scene } from 'react-native-router-flux';
 
-const initialState = {
-  scene: {},
-};
+/**
+ * Global
+ */
+import Header from 'app/scenes/calendar/components/Header'
 
-export default function reducer(state = initialState, action = {}) {
-  switch (action.type) {
-    case ActionConst.FOCUS:
-      return {
-        ...state,
-        scene: action.scene,
-      };
+/**
+ * Sign
+ */
+import SignPhone from 'app/scenes/sign/scenes/signphone'
+import SignUp from 'app/scenes/sign/scenes/signup'
+import SignIn from 'app/scenes/sign/scenes/signin'
 
-    default:
-      return state;
-  }
-}
+/**
+ * Appointments
+ */
+import Calendar from 'app/scenes/calendar'
+import Detail from 'app/scenes/calendar/scenes/detail'
+import New from 'app/scenes/calendar/scenes/new'
+
+/**
+ * Tab icons
+ */
+import {
+  ProfileIcon,
+  DesqerIcon,
+  HistoryIcon
+} from 'app/common/components/Footer'
+
+const scenes = Actions.create(
+  <Scene key="root" hideNavBar={true}>
+    <Scene key="signPhone" component={SignPhone} initial={true} />
+    <Scene key="signUp" component={SignUp} />
+    <Scene key="signIn" component={SignIn} />
+
+    <Scene
+      key="app"
+      tabs={true}
+      tabBarStyle={{ height: 60, backgroundColor: '#BE446D' }}
+    >
+      <Scene key="profile" navBar={Header} component={SignPhone} title="Minha conta" icon={ProfileIcon} />
+      <Scene key="appointments" navBar={Header} icon={DesqerIcon} initial={true}>
+        <Scene key="calendar" component={Calendar} />
+        <Scene key="new" component={New} initial={true} title="Criar serviço" />
+      </Scene>
+      <Scene key="history" navBar={Header} component={SignUp} title="Histórico" icon={HistoryIcon} />
+    </Scene>
+  </Scene>
+)
+
+export default scenes
