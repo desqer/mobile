50fb1f08c7460d670250c94f312f454950009e0f
Diego Schell Fernandes <diego.schell.f@gmail.com>
2017-03-13 08:10:06 -0300

    Serviço padrão de acesso à API

---
+++ app/common/lib/Api.js
@@ -0,0 +1,49 @@
+export default class Api {
+  static headers() {
+    return {
+      'Accept':       'application/json',
+      'Content-Type': 'application/json',
+      'dataType':     'json'
+    }
+  }
+
+  static get(route) {
+    return this.xhr(route, null, 'GET');
+  }
+
+  static put(route, params) {
+    return this.xhr(route, params, 'PUT')
+  }
+
+  static post(route, params) {
+    return this.xhr(route, params, 'POST')
+  }
+
+  static delete(route, params) {
+    return this.xhr(route, params, 'DELETE')
+  }
+
+  static xhr(route, params, verb) {
+    const host = 'http://api.desqer.com'
+    const url = `${host}${route}`
+
+    console.log(url)
+
+    let options = Object.assign({
+      method: verb
+    }, params ? {
+      body: JSON.stringify(params)
+    } : null);
+
+    options.headers = Api.headers()
+    return fetch(url, options).then(resp => {
+      let json = resp.json();
+      if (resp.ok) {
+        return json
+      }
+      return json.then(err => {
+        throw err
+      });
+    }).then(json => json.results);
+  }
+}
